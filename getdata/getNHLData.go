863df568856e9fab023763367806f972d545c83a
Cory Prelerson <cprelerson42@gmail.com>
2023-04-06 10:59:15 -0500

    About to redo main

--- getdata/getNHLData.go
+++ getdata/getNHLData.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 // Get data from the specified URL
@@ -43,117 +42,10 @@ func getNHLData(year string) map[string]interface{} {
 
 	// Return the data
 	return result
-
-	// // Get the dates from the data
-	// dates := result["dates"].([]interface{})
-
-	// // Loop through the dates
-	// for _, date := range dates {
-	// 	// Get the games from the date
-	// 	games := date.(map[string]interface{})["games"].([]interface{})
-
-	// 	// Loop through the games
-	// 	for _, game := range games {
-	// 		// Get the game data
-	// 		gameData := game.(map[string]interface{})
-
-	// 		// Get the game ID
-	// 		gameID := gameData["gamePk"].(float64)
-
-	// 		// Get the game date
-	// 		gameDate := gameData["gameDate"].(string)
-
-	// 		// Get the game status
-	// 		gameStatus := gameData["status"].(map[string]interface{})["abstractGameState"].(string)
-
-	// 		// Get the game type
-	// 		gameType := gameData["gameType"].(string)
-
-	// 		// Get the game season
-	// 		gameSeason := gameData["season"].(string)
-
-	// 		// Get the game teams
-	// 		gameTeams := gameData["teams"].(map[string]interface{})
-
-	// 		// Get the home team
-	// 		homeTeam := gameTeams["home"].(map[string]interface{})["team"].(map[string]interface{})["name"].(string)
-
-	// 		// Get the away team
-	// 		awayTeam := gameTeams["away"].(map[string]interface{})["team"].(map[string]interface{})["name"].(string)
-
-	// 		// Get the game score
-	// 		gameScore := gameData["teams"].(map[string]interface{})["home"].(map[string]interface{})["score"].(float64)
-
-	// 		// Print the game data
-	// 		fmt.Println(gameID, gameDate, gameStatus, gameType, gameSeason, homeTeam, awayTeam, gameScore)
-	// 	}
-	// }
 }
 
 // Main function
 func main() {
 	// Get the NHL data
-	// Use concurrent calls to get data from 2000 to 2020 and store the data in a map
-	data := make(map[string]interface{})
-	for i := 2000; i <= 2020; i++ {
-		data[fmt.Sprintf("%d", i)] = getNHLData(fmt.Sprintf("%d", i))
-	}
-
-	// Output the data to a csv file
-	for _, data := range data {
-		// Get the dates from the data
-		dates := data.(map[string]interface{})["dates"].([]interface{})
-
-		// Loop through the dates
-		for _, date := range dates {
-			// Get the games from the date
-			games := date.(map[string]interface{})["games"].([]interface{})
-
-			// Loop through the games
-			for _, game := range games {
-				// Get the game data
-				gameData := game.(map[string]interface{})
-
-				// Get the game ID
-				gameID := gameData["gamePk"].(int64)
-
-				// Get the game date
-				gameDate := gameData["gameDate"].(string)
-
-				// Get the game status
-				gameStatus := gameData["status"].(map[string]interface{})["abstractGameState"].(string)
-
-				// Get the game type
-				gameType := gameData["gameType"].(string)
-
-				// Get the game season
-				gameSeason := gameData["season"].(string)
-
-				// Get the game teams
-				gameTeams := gameData["teams"].(map[string]interface{})
-
-				// Get the home team
-				homeTeam := gameTeams["home"].(map[string]interface{})["team"].(map[string]interface{})["name"].(string)
-
-				// Get the away team
-				awayTeam := gameTeams["away"].(map[string]interface{})["team"].(map[string]interface{})["name"].(string)
-
-				// Get the game score
-				gameScore := gameData["teams"].(map[string]interface{})["home"].(map[string]interface{})["score"].(float64)
-
-				// Print the game data
-				fmt.Println(gameID, gameDate, gameStatus, gameType, gameSeason, homeTeam, awayTeam, gameScore)
-
-				// Output the game data to a csv file
-				file, err := os.OpenFile("nhl_data.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-				if err != nil {
-					fmt.Println(err)
-				}
-				file.WriteString(fmt.Sprintf("%d,%s,%s,%s,%s,%s,%s,%f", gameID, gameDate, gameStatus, gameType, gameSeason, homeTeam, awayTeam, gameScore))
-				defer file.Close()
-
-			}
-		}
-	}
 
 }
