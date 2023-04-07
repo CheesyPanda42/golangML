ad19a29e99251ad5ddce07b0b84673bbc94a8ff3
Cory Prelerson <cprelerson42@gmail.com>
2023-04-06 22:24:05 -0500

    parseing data

---
+++ parsedata/go.mod
@@ -0,0 +1,3 @@
+module cheesypanda/parsedata
+
+go 1.19
--- parsedata/parseNHLdata.go
+++ parsedata/parseNHLdata.go
@@ -49,8 +49,75 @@ func check(e error) {
 // Create wait group to wait for all goroutines to finish
 var wg sync.WaitGroup
 
+func parseData(year int) {
+	// Print a statement to show that the data is being parsed
+	fmt.Println("Parsing data for year", year)
+
+	defer wg.Done()
+	// Read in the data from the file
+	dataFilePath := "../getdata/data/nhlData_" // Path to the data files
+
+	dataFile, err := os.Open(dataFilePath + fmt.Sprintf("%d", year) + ".json")
+	check(err)
+
+	// Create a variable of type NHLData to hold the data
+	var nhlData NHLData
+
+	// Decode the JSON data into the NHLData struct
+	err = json.NewDecoder(dataFile).Decode(&nhlData)
+	check(err)
+
+	// Create a CSV file to write the data to
+	csvFile, err := os.Create("../getdata/data/nhlData_" + fmt.Sprintf("%d", year) + ".csv")
+	check(err)
+
+	// Write the header row to the CSV file
+	_, err = csvFile.WriteString("homeTeamName,homeTeamScore,awayTeamName,awayTeamScore,homeTeamVowels,awayTeamVowels,homeTeamWon\n")
+	check(err)
+
+	// For each date in the data, loop through the games and create a feature vector and label for each game
+	for _, date := range nhlData.Dates {
+		for _, game := range date.Games {
+			// Get the home team name and score
+			homeTeamName := game.Teams.Home.Team.Name
+			homeTeamScore := game.Teams.Home.Score
+
+			// Get the away team name and score
+			awayTeamName := game.Teams.Away.Team.Name
+			awayTeamScore := game.Teams.Away.Score
+
+			// Create a feature vector that contains the number of vowels in the home team name and the number of vowels in the away team name
+			homeTeamVowels := 0
+			awayTeamVowels := 0
+			for _, char := range homeTeamName {
+				if char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' {
+					homeTeamVowels++
+				}
+			}
+			for _, char := range awayTeamName {
+				if char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' {
+					awayTeamVowels++
+				}
+			}
+
+			// Create a label that is 1 if the home team won and 0 if the home team lost
+			homeTeamWon := 0
+			if homeTeamScore > awayTeamScore {
+				homeTeamWon = 1
+			}
+			// Print a statement to show that the data is being written to the CSV file
+			fmt.Println("Writing data for", homeTeamName, "vs", awayTeamName, "to CSV file")
+
+			// Write the feature vector and label to the CSV file
+			_, err = csvFile.WriteString(fmt.Sprintf("%s,%d,%s,%d,%d,%d,%d\n", homeTeamName, homeTeamScore, awayTeamName, awayTeamScore, homeTeamVowels, awayTeamVowels, homeTeamWon))
+			check(err)
+		}
+	}
+}
+
 // Read in data and write to CSV file
 func main() {
+
 	// Get command line arguments for what years to parse. The first argument is the start year, the second argument is the end year. Convert to integers
 	// Check for the correct number of arguments
 	if len(os.Args) != 3 {
@@ -63,64 +130,11 @@ func main() {
 	endYear, err := strconv.Atoi(os.Args[2])
 	check(err)
 
-	dataFilePath := "../getdata/data/nhlData_" // Path to the data files
-
 	// Loop through the years
 	for i := startYear; i < endYear; i++ {
-		// Read in the data from the file
-		dataFile, err := os.Open(dataFilePath + fmt.Sprintf("%d", i) + ".json")
-		check(err)
-
-		// Create a variable of type NHLData to hold the data
-		var nhlData NHLData
-
-		// Decode the JSON data into the NHLData struct
-		err = json.NewDecoder(dataFile).Decode(&nhlData)
-		check(err)
-
-		// Create a CSV file to write the data to
-		csvFile, err := os.Create("../getdata/data/nhlData_" + fmt.Sprintf("%d", i) + ".csv")
-		check(err)
-
-		// Write the header row to the CSV file
-		_, err = csvFile.WriteString("homeTeamName,homeTeamScore,awayTeamName,awayTeamScore,homeTeamVowels,awayTeamVowels,homeTeamWon\n")
-		check(err)
-
-		// For each date in the data, loop through the games and create a feature vector and label for each game
-		for _, date := range nhlData.Dates {
-			for _, game := range date.Games {
-				// Get the home team name and score
-				homeTeamName := game.Teams.Home.Team.Name
-				homeTeamScore := game.Teams.Home.Score
-
-				// Get the away team name and score
-				awayTeamName := game.Teams.Away.Team.Name
-				awayTeamScore := game.Teams.Away.Score
-
-				// Create a feature vector that contains the number of vowels in the home team name and the number of vowels in the away team name
-				homeTeamVowels := 0
-				awayTeamVowels := 0
-				for _, char := range homeTeamName {
-					if char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' {
-						homeTeamVowels++
-					}
-				}
-				for _, char := range awayTeamName {
-					if char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' {
-						awayTeamVowels++
-					}
-				}
-
-				// Create a label that is 1 if the home team won and 0 if the home team lost
-				homeTeamWon := 0
-				if homeTeamScore > awayTeamScore {
-					homeTeamWon = 1
-				}
-
-				// Write the feature vector and label to the CSV file
-				_, err = csvFile.WriteString(fmt.Sprintf("%s,%d,%s,%d,%d,%d,%d\n", homeTeamName, homeTeamScore, awayTeamName, awayTeamScore, homeTeamVowels, awayTeamVowels, homeTeamWon))
-				check(err)
-			}
-		}
+		wg.Add(1)
+		// Create a go routine to read in the data and write it to a CSV file
+		go parseData(i)
 	}
+	wg.Wait()
 }
